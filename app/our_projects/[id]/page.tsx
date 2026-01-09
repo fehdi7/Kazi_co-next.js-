0af4af4d01be0c9997d1f434421a434798b0bff3
Fehdi <temporarylang@Fehdis-MacBook-Air.local>
2026-01-09 10:56:46 +0400

    your commit message

--- app/our_projects/[id]/page.tsx
+++ app/our_projects/[id]/page.tsx
@@ -1,18 +1,28 @@
-import React from 'react'
 import prisma from '@/src/lib/prisma'
 
-async function page() {
-    const posts = await prisma.post.findMany()
+export default async function Page({
+  params,
+}: {
+  params: { id: string }
+}) {
+  const id = Number(params.id)
+
+  if (isNaN(id)) {
+    return <div>Invalid post ID</div>
+  }
+
+  const post = await prisma.post.findUnique({
+    where: { id },
+  })
+
+  if (!post) {
+    return <div>Post not found</div>
+  }
+
   return (
-      <main className="p-4 my-10 border-amber-900 border-2 rounded-lg">  
-    
-    <ul>
-      {posts.map(post => (
-        <li key={post.id}>This is {post.title}</li>
-      ))}
-    </ul>
+    <main className="p-4 my-10 border-amber-900 border-2 rounded-lg">
+      <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
+      <p className="text-lg">{post.content}</p>
     </main>
   )
 }
-
-export default page
